88b4b416b217b6848f3bf154baaa45f8ea4b451f
Oliver Ripps <oripps@stuy.edu>
2018-11-04 14:46:33 -0500

    ordered constructors

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,19 +1,10 @@
 import java.util.ArrayList;
 public class OrderedArrayList<T> extends NoNullArrayList<T>{
-  public NoNullArrayList(){
-      super();
-    }
-  public NoNullArrayList(int startingCapacity){
+  public OrderedArrayList(){
+    super();
+  }
+
+  public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
-    }
-  public E set(int index, E element) {
-    if (element == null) {
-      throw new IllegalArgumentException();
-      }
-    return super.set(index, element);}
-
-    }
-
-
-
+  }
 }
