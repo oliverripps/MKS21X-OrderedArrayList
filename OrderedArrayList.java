f08b715a5473cc586b9a0a8695b35898e07bb7dc
Oliver Ripps <oripps@stuy.edu>
2018-11-04 14:57:23 -0500

    add working?

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,15 @@ public class OrderedArrayList<T> extends NoNullArrayList<T>{
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
+  public boolean add(T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("Cannot add null");}
+    for (int i = 0; i < size(); i++) {
+      if (String.valueOf(value).compareTo(String.valueOf(this.get(i))) <= 0) {
+       super.add(i, value);
+       return true;}
+     }
+    super.add(0, value);
+    return true;
+ }
 }
