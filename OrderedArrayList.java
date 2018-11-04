273b0c5a5c9841b61396c446b4d1430d57eb8310
Oliver Ripps <oripps@stuy.edu>
2018-11-04 14:43:44 -0500

    first no null function

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,19 @@
 import java.util.ArrayList;
 public class OrderedArrayList<T> extends NoNullArrayList<T>{
+  public NoNullArrayList(){
+      super();
+    }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+    }
+  public E set(int index, E element) {
+    if (element == null) {
+      throw new IllegalArgumentException();
+      }
+    return super.set(index, element);}
+
+    }
+
+
+
 }
